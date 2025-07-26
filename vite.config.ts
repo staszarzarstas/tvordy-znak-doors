f4c5bbbef34c951e0b01b75bf97030fcfb0c769f
Stanislav <royultra77@gmail.com>
2025-07-26 17:24:31 +0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: true,
     port: 3000,
     open: true,
     strictPort: true,
   },
-  base: '/tvordy-znak-doors-website-main/',
+  base: '/',
   plugins: [
     react(),
   ],
@@ -24,4 +24,4 @@ export default defineConfig(({ mode }) => ({
   build: {
     assetsInlineLimit: 0,
   },
-}));
+});
